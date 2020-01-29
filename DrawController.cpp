f9510be4bdd3b0b5aead665355f5cd6a1484c057
182719 <joakim-a@hotmail.com>
2020-01-29 11:30:42 +0100

    Fixed GDI leaks in drawController

--- DrawController.cpp
+++ DrawController.cpp
@@ -5,25 +5,40 @@
 void drawTrafficLight(HDC hdc, int x, int y, TrafficLight trafficLight)
 {
 	// GDI logic for creating a brush
-	HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 0));
-	HGDIOBJ hOrg = SelectObject(hdc, hBrush);
+	HBRUSH blackBrush = CreateSolidBrush(RGB(0, 0, 0));
+	HBRUSH greyBrush = CreateSolidBrush(RGB(192, 192, 192));
+	HBRUSH redBrush = CreateSolidBrush(RGB(255, 0, 0));
+	HBRUSH yellowBrush = CreateSolidBrush(RGB(255, 255, 0));
+	HBRUSH greenBrush = CreateSolidBrush(RGB(0, 255, 0));
+	HBRUSH actualBrush = blackBrush;
+
+	HGDIOBJ hOrg = SelectObject(hdc, actualBrush);
+	
+
 	// Red ellipse (circle)
 	Rectangle(hdc, x, y, x + 100, y + 270);
 	
 	//øverste lys
-	hBrush = trafficLight.getCurrentState().red ? CreateSolidBrush(RGB(255, 0, 0)) : CreateSolidBrush(RGB(192, 192, 192));
-	SelectObject(hdc, hBrush);
+	actualBrush = trafficLight.getCurrentState().red ? redBrush : greyBrush;
+	SelectObject(hdc, actualBrush);
 	Ellipse(hdc, x + 10, y + 10, x + 90, y + 80);
 
-	hBrush = trafficLight.getCurrentState().yellow ? CreateSolidBrush(RGB(255, 255, 0)) : CreateSolidBrush(RGB(192, 192, 192));
-	SelectObject(hdc, hBrush);
+	actualBrush = trafficLight.getCurrentState().yellow ? yellowBrush : greyBrush;
+	SelectObject(hdc, actualBrush);
 	Ellipse(hdc, x + 10, y + 100, x + 90, y + 170);
 
-	hBrush = trafficLight.getCurrentState().green ? CreateSolidBrush(RGB(0, 255, 0)) : CreateSolidBrush(RGB(192, 192, 192));
-	SelectObject(hdc, hBrush);
+	actualBrush = trafficLight.getCurrentState().green ? greenBrush : greyBrush;
+	SelectObject(hdc, actualBrush);
 	Ellipse(hdc, x + 10, y + 190, x + 90, y + 260);
 
 	// Cleanup, set back original brush
 	SelectObject(hdc, hOrg);
-	DeleteObject(hBrush);
+	DeleteObject(actualBrush);
+	DeleteObject(blackBrush);
+	DeleteObject(greyBrush);
+	DeleteObject(redBrush);
+	DeleteObject(yellowBrush);
+	DeleteObject(greenBrush);
+	DeleteObject(hOrg);
+
 }
