4e9008d399542bddf0e846065dc606e8c0b5e0c6
182719 <joakim-a@hotmail.com>
2020-01-29 11:33:06 +0100

    Updated comments DrawController.cpp

--- DrawController.cpp
+++ DrawController.cpp
@@ -4,7 +4,7 @@
 
 void drawTrafficLight(HDC hdc, int x, int y, TrafficLight trafficLight)
 {
-	// GDI logic for creating a brush
+	// GDI logic for creating all brushes
 	HBRUSH blackBrush = CreateSolidBrush(RGB(0, 0, 0));
 	HBRUSH greyBrush = CreateSolidBrush(RGB(192, 192, 192));
 	HBRUSH redBrush = CreateSolidBrush(RGB(255, 0, 0));
@@ -12,13 +12,13 @@ void drawTrafficLight(HDC hdc, int x, int y, TrafficLight trafficLight)
 	HBRUSH greenBrush = CreateSolidBrush(RGB(0, 255, 0));
 	HBRUSH actualBrush = blackBrush;
 
+	// Saving original brush to be put back later
 	HGDIOBJ hOrg = SelectObject(hdc, actualBrush);
 	
-
-	// Red ellipse (circle)
+	// Drawing a traffic light rectangle
 	Rectangle(hdc, x, y, x + 100, y + 270);
 	
-	//øverste lys
+	// Drawing the lights in the correct color
 	actualBrush = trafficLight.getCurrentState().red ? redBrush : greyBrush;
 	SelectObject(hdc, actualBrush);
 	Ellipse(hdc, x + 10, y + 10, x + 90, y + 80);
@@ -31,7 +31,8 @@ void drawTrafficLight(HDC hdc, int x, int y, TrafficLight trafficLight)
 	SelectObject(hdc, actualBrush);
 	Ellipse(hdc, x + 10, y + 190, x + 90, y + 260);
 
-	// Cleanup, set back original brush
+	// Setting the original brush back, deleting all brushes 
+	// TODO: Possible to persist brushes? 
 	SelectObject(hdc, hOrg);
 	DeleteObject(actualBrush);
 	DeleteObject(blackBrush);
